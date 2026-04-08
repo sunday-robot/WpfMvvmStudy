46b49ca0dde76a50b2e326afcb1a4fdd895e63c1
sunday robot <sgx03150@nifty.com>
2026-04-08 23:13:46 +0900

    a

--- IdealAppStudy/ChangedProperty.cs
+++ IdealAppStudy/ChangedProperty.cs
@@ -1,3 +1,8 @@
 ﻿namespace IdealAppStudy;
 
+/// <summary>
+/// 更新されたプロパティ(名前と新しい値)
+/// </summary>
+/// <param name="Name"></param>
+/// <param name="Value"></param>
 public record ChangedProperty(string Name, object Value);
